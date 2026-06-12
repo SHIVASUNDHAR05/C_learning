9a6cc035359cbeeee18b4ca647b095a38854ac70
SHIVASUNDHAR05 <shivasundhar05@gmail.com>
2026-06-12 22:25:04 +0530

    problems1

---
+++ c_assessment3/problem_11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter Number:");
+    scanf("%d",&a);
+    if ((a/10)<=(a%10))
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+}
