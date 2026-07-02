72008ea652b3f1f84678159daecf3934af184848
SHIVASUNDHAR05 <shivasundhar05@gmail.com>
2026-07-02 23:38:33 +0530

    begin6

---
+++ c_assessment4/problem_15.c
@@ -0,0 +1,24 @@
+//write a loop program to reduce the first digit if last digit is odd number
+
+#include <stdio.h>
+int main()
+{
+    int a,count,b,first_digit;
+    a=423452;
+    b=a;
+    count=1;
+    if ((a%2)==1)
+    {
+       loop:if (a>9)
+       {
+           count=count*10;
+           a=a/10;
+           goto loop;
+        }
+        first_digit=b-count;
+        printf("%d",first_digit);
+   }
+   else{
+    printf("%d",b);
+   }
+}
